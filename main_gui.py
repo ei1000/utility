113aaedb07177701fc0e723f77e56fbdeb4f219d
Einar <einar.martin.bernsen@gmail,com>
2024-10-05 21:39:33 +0200

    Made it component based and moved pdf2markdown class to its dir

--- main_gui.py
+++ main_gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import customtkinter as ctk
 import os
+from pdf_2_markdown.pd2markdown_class import Pdf2Markdown
 
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("green")
@@ -22,24 +23,26 @@ class App(ctk.CTk):
         container = ctk.CTkFrame(self)
         container.pack(fill="both", expand=True)
 
+        #Add the script classes here in list below and it will generate dictionaries
         self.frame_classes= [MainMenu, Pdf2Markdown, ScriptTemplate]
+        self.frame_classes= {F.__name__: F for F in self.frame_classes}
 
         self.frames = {}
         # Initialize the frames and store them in a dictionary
-        for F in self.frame_classes:
+        for frame_name, F in self.frame_classes.items():
             frame = F(container, self)
-            self.frames[F] = frame
+            self.frames[frame_name] = frame
             frame.grid(row=0, column=0, sticky="nsew")
 
         #Center grid
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         
-        # Show the main menu initially
-        self.show_frame(MainMenu)
+        #Show the main menu on startup
+        self.show_frame('MainMenu')
 
         
-    #Function to switch frame
+    #Function to switch frame based on frame_name
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
@@ -54,95 +57,32 @@ class MainMenu(ctk.CTkFrame):
         label.pack(pady=20, anchor='center')
 
         #Creating a button for every script
-        for F in controller.frame_classes[1:]:
-            button = ctk.CTkButton(self, text=f"{F.__name__}", 
-                                    command=lambda F=F: controller.show_frame(F))
+        for frame_name in list(controller.frame_classes.keys())[1:]:
+            button = ctk.CTkButton(self, text=f'{frame_name}', 
+                                    command=lambda frame_name=frame_name: controller.show_frame(frame_name))
             button.pack(pady=10, anchor='center')
         
         #Quit button
-        quit_button = ctk.CTkButton(self, text="Quit", command=controller.quit)
-        quit_button.pack(pady=10, anchor='center')
+        quit_button = ctk.CTkButton(self, text="Quit", command=controller.quit, fg_color='#C3423F', hover_color='#8E2E2C')
+        quit_button.place(relx=1, rely=0, anchor='ne', x=-10, y=10)
 
-# Script 1 Frame
-class Pdf2Markdown(ctk.CTkFrame):
-    def __init__(self, parent, controller):
-        super().__init__(parent)
 
-        # Script 1 UI
-        script_label = ctk.CTkLabel(self, text="Script 1", font=('Helvetica', 20))
-        script_label.pack(pady=10)
-        
-        arg_label = ctk.CTkLabel(self, text="Select input file: ")
-        arg_label.pack(pady=10)
 
-        # File 1 selection
-        self.selected_input_file = ctk.CTkLabel(self, text='No file selected')
-        self.selected_input_file.pack(pady=10)
-
-        browse_button = ctk.CTkButton(self, text='Browse files', command=lambda: self.browse_files(self.selected_input_file))
-        browse_button.pack(pady=10)
-
-        # File 2 selection
-        arg_label2 = ctk.CTkLabel(self, text="Select input folder: ")
-        arg_label2.pack(pady=10)
-
-        self.selected_input_file2 = ctk.CTkLabel(self, text='No folder selected')
-        self.selected_input_file2.pack(pady=10)
-
-        browse_button2 = ctk.CTkButton(self, text='Browse folders', command=lambda: self.browse_files(self.selected_input_file2, select_type='folder'))
-        browse_button2.pack(pady=10)
-
-        # Run script button
-        run_button = ctk.CTkButton(self, text="Run Script 1", command=self.execute_script_1)
-        run_button.pack(pady=10)
-
-        # Back to main menu button
-        back_button = ctk.CTkButton(self, text="Back to Main Menu", command=lambda: controller.show_frame(MainMenu))
-        back_button.pack(side='bottom', pady=10, fill='x')
-
-    def browse_files(self, input_file_label, select_type='file'):
-        """Handles file/folder browsing and updates the label"""
-        if select_type == 'file':
-            filename = ctk.filedialog.askopenfilename(title='Select a File',
-                                                  filetypes=(('PDF files', '*.pdf*'), ('all files', '*.*')))
-        elif select_type == 'folder':
-            filename = ctk.filedialog.askdirectory(title='Select a folder')
-
-        if filename:
-            input_file_label.configure(text=filename)
-    
-    def execute_script_1(self):
-        """Executes the script with the selected file and folder"""
-        file_path_1 = self.selected_input_file.cget("text") 
-        file_path_2 = self.selected_input_file2.cget("text")
-        
-        if file_path_1 == 'No file selected' or file_path_2 == 'No folder selected':
-            tk.messagebox.showwarning("Warning", "Please select both a file and a folder.")
-        else:
-            # Run the external script (replace this with your own script path)
-            os.system(f"python pdf_2_markdown/pdf_2_markdown.py {file_path_1} {file_path_2}")
-            tk.messagebox.showinfo("Pdf converted to markdown", f"File can be found in: {file_path_2}")
-
-# Script Template Frame
+# Script Template Frame for later expansion
 class ScriptTemplate(ctk.CTkFrame):
     def __init__(self, parent, controller):
         super().__init__(parent)
 
+        #Top text
         label = ctk.CTkLabel(self, text="Script Template", font=('Helvetica', 20))
         label.pack(pady=20)
-
-        browse_button = ctk.CTkButton(self, text="Browse Files", command=self.browse_files)
-        browse_button.pack(pady=10)
         
         back_button = ctk.CTkButton(self, text="Back to Main Menu", 
-                                    command=lambda: controller.show_frame(MainMenu))
-        back_button.pack(pady=10)
+                                    command=lambda: controller.show_frame('MainMenu'))
+        back_button.pack(side='bottom', pady=20, fill='x')
 
-    def browse_files(self):
-        filename = tk.filedialog.askopenfilename(title="Select a File")
-        if filename:
-            tk.messagebox.showinfo("Selected File", f"File: {filename}")
 
+#Make the program run when called. Start the loop.
 if __name__ == "__main__":
     app = App()
     app.mainloop()
---
+++ pdf_2_markdown/pd2markdown_class.py
@@ -0,0 +1,69 @@
+import customtkinter as ctk
+import tkinter as tk
+import os
+
+# Script 1 Frame
+class Pdf2Markdown(ctk.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+
+        # Script 1 UI
+        script_label = ctk.CTkLabel(self, text="Script 1", font=('Helvetica', 20))
+        script_label.pack(pady=10)
+        
+        arg_label = ctk.CTkLabel(self, text="Select input file: ")
+        arg_label.pack(pady=10)
+
+        # File 1 selection
+        self.selected_input_file = ctk.CTkLabel(self, text='No file selected')
+        self.selected_input_file.pack(pady=10)
+
+        browse_button = ctk.CTkButton(self, text='Browse files', command=lambda: self.browse_files(self.selected_input_file))
+        browse_button.pack(pady=10)
+
+        # File 2 selection
+        arg_label2 = ctk.CTkLabel(self, text="Select input folder: ")
+        arg_label2.pack(pady=10)
+
+        self.selected_input_file2 = ctk.CTkLabel(self, text='No folder selected')
+        self.selected_input_file2.pack(pady=10)
+
+        browse_button2 = ctk.CTkButton(self, text='Browse folders', command=lambda: self.browse_files(self.selected_input_file2, select_type='folder'))
+        browse_button2.pack(pady=10)
+
+        # Run script button
+        run_button = ctk.CTkButton(self, text="Run Script 1", command=self.execute_script_1)
+        run_button.pack(pady=10)
+
+        # Back to main menu button
+        back_button = ctk.CTkButton(self, text="Back to Main Menu", command=lambda: controller.show_frame('MainMenu'))
+        back_button.pack(side='bottom', pady=20, fill='x')
+
+    def browse_files(self, input_file_label, select_type='file'):
+        
+        #If we want to find a file then it needs to be a pdf. Else if it is a folder just select a folder
+        if select_type == 'file':
+            filename = ctk.filedialog.askopenfilename(title='Select a File',
+                                                  filetypes=(('PDF files', '*.pdf*'), ('all files', '*.*')))
+        elif select_type == 'folder':
+            filename = ctk.filedialog.askdirectory(title='Select a folder')
+
+        #If a file was chosen, here is the path
+        if filename:
+            input_file_label.configure(text=filename)
+    
+    #Executing the script
+    def execute_script_1(self):
+        #The paths
+        file_path_1 = self.selected_input_file.cget("text") 
+        file_path_2 = self.selected_input_file2.cget("text")
+        
+        #A check incase only one is selected
+        if file_path_1 == 'No file selected' or file_path_2 == 'No folder selected':
+            tk.messagebox.showwarning("Warning", "Please select both a file and a folder.")
+        
+        #Run the script in the terminal and then give a pop-up for the user
+        #Not decided if I want to keep using terminal here or just import the function instead.
+        else:
+            os.system(f"python pdf_2_markdown/pdf_2_markdown.py {file_path_1} {file_path_2}")
+            tk.messagebox.showinfo("Pdf converted to markdown", f"File can be found in: {file_path_2}")
